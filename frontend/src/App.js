c9edb3b072806bc08f87279b92fbafad3ad8683c
Lahiru heshan <lahiruheshan454@gmail.com>
2024-09-25 16:31:54 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/whatsapp/whatsapp';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+ 
+       
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
